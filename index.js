1493d1c3c87b65b95920956b3982a8b9f6334d0d
Dakota Vikdal <dakota.james19@hotmail.com>
2022-12-09 15:09:20 -0500

    commit tree 00914f89624b713b84fd141cd26c33c6faa36387

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   return cats.push(name)
+}
+
+function  destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function  destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.push(name)
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCat = cats.slice()
+    copyOfCat.unshift(name)
+    return copyOfCat
+}
+
+function removeLastCat() {
+    const copyOfKitty = cats.slice()
+    copyOfKitty.pop()
+    return copyOfKitty
+}
+
+function removeFirstCat() {
+    const copyOfKitties = cats.slice()
+    copyOfKitties.shift()
+    return copyOfKitties
+}
